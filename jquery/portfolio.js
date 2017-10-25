d1bc8e5c0bfa1d85a4551daed335a9fa65241a1d
Alejandro <92.alejandrogarcia@gmail.com>
2017-10-25 20:47:40 +0200

    project_gallery enhancement

--- jquery/portfolio.js
+++ jquery/portfolio.js
@@ -6,10 +6,6 @@ $(document).ready(function(){
     $("#wrapper > div").addClass('hidden');
     $("#1").removeClass('hidden');
     $("li:has(#pag1,#pag1_2,#pag1,#pag1_2_2)").addClass('active');
-    $("#next").click(function(){
-      $("#wrapper > div").addClass('hidden');
-      $("#2").removeClass('hidden');
-    });
   });
 
 $("#pag1,#pag1_2").click(function(){
@@ -56,6 +52,58 @@ $("#pag6,#pag6_2").click(function(){
   $("#6").removeClass('hidden');
   $("li:not(:has(#pag6,#pag6_2))").removeClass('active');
   $("li:has(#pag6,#pag6_2)").addClass('active');
+});
+
+$(document).ready(function(){
+    $("#wrapper2 > div").addClass('hidden');
+    $("#1_2").removeClass('hidden');
+    $("li:has(#pag1_2_2,#pag1_2_2_2)").addClass('active');
+  });
+
+$("#pag1_2_2,#pag1_2_2_2").click(function(){
+  $("#wrapper2 > div").addClass('hidden');
+  $("#1_2").removeClass('hidden');
+  $("li:not(:has(#pag1_2_2,#pag1_2_2_2))").removeClass('active');
+  $("li:has(#pag1_2_2,#pag1_2_2_2)").addClass('active');
+});
+
+
+$("#pag2_2_2,#pag2_2_2_2").click(function(){
+  $("#wrapper2 > div").addClass('hidden');
+  $("#2_2").removeClass('hidden');
+  $("li:not(:has(#pag2_2_2,#pag2_2_2_2))").removeClass('active');
+  $("li:has(#pag2_2_2,#pag2_2_2_2)").addClass('active');
+});
+
+
+$("#pag3_2_2,#pag3_2_2_2").click(function(){
+  $("#wrapper2 > div").addClass('hidden');
+  $("#3_2").removeClass('hidden');
+  $("li:not(:has(#pag3_2_2,#pag3_2_2_2))").removeClass('active');
+  $("li:has(#pag3_2_2,#pag3_2_2_2)").addClass('active');
+});
+
+
+$("#pag4_2_2,#pag4_2_2_2").click(function(){
+  $("#wrapper2 > div").addClass('hidden');
+  $("#4_2").removeClass('hidden');
+  $("li:not(:has(#pag4_2_2,#pag4_2_2_2))").removeClass('active');
+  $("li:has(#pag4_2_2,#pag4_2_2_2)").addClass('active');
+});
+
+
+$("#pag5_2_2,#pag5_2_2_2").click(function(){
+  $("#wrapper2 > div").addClass('hidden');
+  $("#5_2").removeClass('hidden');
+  $("li:not(:has(#pag5_2_2,#pag5_2_2_2))").removeClass('active');
+  $("li:has(#pag5_2_2,#pag5_2_2_2)").addClass('active');
+});
+
+$("#pag6_2_2,#pag6_2_2_2").click(function(){
+  $("#wrapper2 > div").addClass('hidden');
+  $("#6_2").removeClass('hidden');
+  $("li:not(:has(#pag6_2_2,#pag6_2_2_2))").removeClass('active');
+  $("li:has(#pag6_2_2,#pag6_2_2_2)").addClass('active');
 });
 
 
